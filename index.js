5308c84642be6e6fba91596b6425e635988e7459
MrCamoga <mrcamoga@gmail.com>
2025-04-30 15:23:27 +0200

    Added UN recognized countries

--- index.js
+++ index.js
@@ -1,23 +1,16 @@
-const cardTemplate = function (/* You can pass the data here*/) {
-  return `<div class="card">
-              <img id="flag-image" src="ADD THE IMAGE LINK HERE" alt="flag" />
-              <h1 class="center">ADD COUNTRY NAME HERE</h1>
+const cardTemplate = function (imgUrl, countryName) {
+	return `<div class="card">
+              <img id="flag-image" src="${imgUrl}" alt="flag" />
+              <h1 class="center">${countryName}</h1>
             </div>`;
 };
 
 const countriesNode = document.getElementById("countries");
 
-fetch(/* Need the provide API URL to get all countries */)
-  .then(function (response) {
-    // fetch() returns a promise containing the response (a Response object).
-    // This is just an HTTP response, not the actual JSON. 
-    // To extract the JSON body content from the response, 
-    // we use the json() method and pass it into the next .then()
-  })
-  .then(function (countries) {
-    // Here is where you'll need to add into the DOM all the countries received from API 
-
-    // 1 - We will need to iterate the countries variable with a loop
-    // 2 - You can use the cardTemplate() function to create a div with a class card already styled
-    // 💡 you can use countriesNode variable to add elements
-  });
+fetch("https://restcountries.com/v3.1/independent?fields=flags,name")
+	.then(function (response) {
+		return response.json();
+	})
+	.then(function (countries) {
+		countries.forEach((country) => (countriesNode.innerHTML += cardTemplate(country.flags.svg, country.name.common)));
+	});
